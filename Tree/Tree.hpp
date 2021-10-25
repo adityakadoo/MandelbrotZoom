05ea962065262fca89e566a9ddf62cd414830a6d
adityakadoo <aditya1449kadoo@gmail.com>
2021-10-26 02:03:30 +0530

    📦 NEW: Tree class added

--- Tree/Tree.hpp
+++ Tree/Tree.hpp
@@ -1,56 +1,69 @@
 #ifndef __TREE__
 #define __TREE__
 #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+typedef long long ll;
 
-#define a 2
-#define b 4
-
-template<typename T>
-class Node
-{
+template <typename T, int b>
+class Node{
 public:
     /* Public data and connected nodes */
-    Node<T>* parent;
-    Node<T>** children;
-    T data;
+    pair<Node<T, b>*,ll> parent;
+    Node<T, b> **children;
+    T **data;
+    ll size;
     /* Constructors and Destructors */
-    // Default constructor
-    Node();
-    // Constructor with given data
-    Node(T);
+    // Constructor with parent pointer
+    Node(Node<T, b> *,ll);
     // Default Destructor
     ~Node();
 };
 
-#define parent(n) n->parents.front()
-#define left(n) n->children.front()
-#define right(n) n->children.back()
-#define data(n) n->data
-enum traversal_type {PRE, POST, IN, EULER};
+#define parent(n) n->parent
+#define child(n, i) n->children[i]
+#define data(n,i) n->data[i]
+#define size(n) n->size
 
-template<typename K,typename V>
-class Tree
-{
+template <typename T, int b>
+class Tree{
 private:
-    Node<T>* root;
+    Node<T, b> *root;
+
+    // Binary Search for internal use
+    static ll binarySearch(T **, ll, T);
+    // Recursive Printer for internal use
+    static void printNode(Node<T, b>*, string s); 
+
 public:
     // Default Constructor
-    Tree(): root(NULL){}
+    Tree();
+    // Default Destructor
+    ~Tree();
 
     /* Tree Operations */
-    // Inserts the given element into the tree
-    bool insert(T, bool);
-    // Find the given key
-    Node<T>* find(T);
-    //
-    Node<T>* min();
-    Node<T>* max();
-    Node<T>* succ(Node<T>*);
-    Node<T>* pred(Node<T>*);
-    bool remove(T);
-    void traverse(traversal_type,function<void(Node<T>*)>,Node<T>*);
-    friend ostream& operator<<(std::ostream&, Tree<T>&);
+    // Inserts the given element
+    bool insert(const T&);
+    // Find the given element
+    pair<Node<T,b>*,ll> find(T);
+    // Find min element in the tree
+    pair<Node<T,b>*,ll> min();
+    // Find max element in the tree
+    pair<Node<T,b>*,ll> max();
+    // Find successor of the given node
+    pair<Node<T,b>*,ll> succ(Node<T,b>*,ll);
+    // Find predecessor of the given node
+    pair<Node<T,b>*,ll> pred(Node<T,b>*,ll);
+    // // Remove the given element
+    // bool remove(T);
+    // Traversal based on given type
+    void traverse(function<void(T*)> f, Node<T, b> *r=NULL);
+    // Print the tree in a pretty format
+    friend ostream& operator<<(std::ostream &cout, Tree<T, b> &t){
+        cout<<"┬\n";
+        t.printNode(t.root,"");
+        return cout;
+    }
 };
 
 #endif
