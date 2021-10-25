566146f1b104b75ff9e4c64d5c5871c303445e8d
adityakadoo <aditya1449kadoo@gmail.com>
2021-10-26 02:04:31 +0530

    👌 IMPROVE: Map implemented using Tree

--- Map/Map.cpp
+++ Map/Map.cpp
@@ -1,46 +1,62 @@
 #include "Map.hpp"
 
-template<typename K,typename V>
-Map<K,V>::Map(function<V(K)> h): hashing_func(h){
-    entries = new Entry<K,V>[MAX_ENTRIES];
+template<typename H,typename V>
+Entry<H,V>::Entry(H h1, V v1): h(h1), v(v1){}
+
+template<typename H,typename V>
+Entry<H,V>::Entry(const Entry& e): h(e.h), v(e.v){}
+
+template<typename H,typename V>
+Entry<H,V>::~Entry(){}
+
+template<typename H,typename V>
+bool Entry<H,V>::operator<(const Entry<H,V>& e){
+    return h<e.h;
 }
 
-template<typename K,typename V>
-Map<K,V>::~Map(){
-    delete[] entries;
+template<typename H,typename V>
+bool Entry<H,V>::operator<=(const Entry<H,V>& e){
+    return h<=e.h;
 }
 
-template<typename K,typename V>
-bool Map<K,V>::create(K& key){
-    if(size==MAX_ENTRIES)
-        return false;
-    entries[size] = new Entry<K,V>;
-    entries[size].key = key;
-    entries[size].value = hashing_func(key);
-    size++;
-    return true;
+template<typename H,typename V>
+bool Entry<H,V>::operator==(const Entry<H,V>& e){
+    return h==e.h;
 }
 
-template<typename K,typename V>
-Entry<K,V>* Map<K,V>::get(K& key){
-    // Simple Linear Search
-    for(long long i=0;i<size;i++){
-        if(entries[i].key==key)
-            return entries[i];
-    }
-    return NULL;
+template<typename H,typename V>
+bool Entry<H,V>::operator!=(const Entry<H,V>& e){
+    return h!=e.h;
 }
 
-template<typename K,typename V>
-Entry<K,V>* Map<K,V>::getOrCreate(K& key){
-    Entry<K,V>* res = get(key);
-    if(res)
-        return res;
-    if(size==MAX_ENTRIES)
-        return NULL;
-    entries[size] = new Entry<K,V>;
-    entries[size].key = key;
-    entries[size].value = hashing_func(key);
-    size++;
-    return entries[size-1];
+template<typename H,typename V>
+bool Entry<H,V>::operator>=(const Entry<H,V>& e){
+    return h>=e.h;
+}
+
+template<typename H,typename V>
+bool Entry<H,V>::operator>(const Entry<H,V>& e){
+    return h>e.h;
+}
+
+
+template<typename K,typename H,typename V>
+Map<K,H,V>::Map(function<H(K)> h): hashing_func(h){
+    t = new Tree<Entry<H,V>, 4>();
+}
+
+template<typename K,typename H,typename V>
+Map<K,H,V>::~Map(){
+    delete t;
+}
+
+template<typename K,typename H,typename V>
+pair<Entry<H,V>*,bool> Map<K,H,V>::getOrCreate(K key, V value){
+    Entry<H,V> temp(hashing_func(key),value);
+    pair<Node<Entry<H,V>, 4>*, ll> res = t->find(temp);
+    if(res.first)
+        return make_pair(data(res.first,res.second),false);
+    t->insert(temp);
+    res = t->find(temp);
+    return make_pair(data(res.first,res.second),true);
 }
--- Map/Map.hpp
+++ Map/Map.hpp
@@ -1,40 +1,65 @@
 #ifndef __MANDELBROT_MAP__
 #define __MANDELBROT_MAP__
 #include <bits/stdc++.h>
+#include "../Tree/Tree.hpp"
+#include "../Tree/Tree.cpp"
 using namespace std;
 
-#define MAX_ENTRIES 2'00'000
-
 // A class to denote an entry of the map
-template<typename K, typename V>
+template<typename H, typename V>
 class Entry{
 public:
-    K key; // Key
-    V value; // Value
+    H h; // Hashed key
+    V v; // Value
+
+    /* Constructors and Destructors */
+    // Constructor with given hash and value
+    Entry(H,V);
+    // Copy Constructor
+    Entry(const Entry<H,V>&);
+    // Default destructor
+    ~Entry();
+
+    /* Comparison operators */
+    bool operator<(const Entry<H,V>&);
+    bool operator<=(const Entry<H,V>&);
+    bool operator==(const Entry<H,V>&);
+    bool operator!=(const Entry<H,V>&);
+    bool operator>=(const Entry<H,V>&);
+    bool operator>(const Entry<H,V>&);
+
+    // Pretty printer
+    friend ostream& operator<<(std::ostream &cout, Entry<H,V> &e){
+        cout<<"{ "<<e.h<<" : "<<e.v<<" }";
+        return cout;
+    }
 };
 
-template<typename K,typename V>
-class Map
-{
+#define hash(e) e->h
+#define value(e) e->v
+
+template<typename K,typename H,typename V>
+class Map{
 private:
-    Entry<K,V>* entries;
-    long long size;
-    function<V(K)> hashing_func;
+    Tree<Entry<H,V>,4> *t;
+    function<H(K)> hashing_func;
 public:
     /* Constructors and Destructors */
     // Default Constructor
-    Map(function<V(K)>);
+    Map(function<H(K)>);
     // Default Destructor
     ~Map();
 
     /* Map operations */
-    // Inserts new key in the map
-    bool create(K);
-    // Searches for given key in the map
-    Entry<K,V>* get(K&);
-    // Searches for the given key in the map
-    // If not found then creates a new one
-    Entry<K,V>* getOrCreate(K&);
+    // If new key then inserts in the map
+    // else return the entry for an old key
+    pair<Entry<H,V>*,bool> getOrCreate(K,V);
+
+    // Pretty printer
+    friend ostream& operator<<(std::ostream &cout, Map<K,H,V> &m){
+        cout<<*(m.t);
+        return cout;
+    }
 };
 
 #endif
