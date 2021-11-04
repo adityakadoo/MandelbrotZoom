5bac92a9287a9b42e00f74dff825c7300544600a
adityakadoo <aditya1449kadoo@gmail.com>
2021-11-04 23:06:03 +0530

    🐛 FIX: Including headers in Mandelbrot.cpp

--- include/Mandelbrot.hpp
+++ include/Mandelbrot.hpp
@@ -4,12 +4,12 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <iostream>
-#include "../include/Complex.hpp"
+#include <Complex.hpp>
 
 #define MAX_ITER 500
 
 // Implements the mandelbrot escape-time algorithm
-long double mandelbrot(Complex<long double>);
+long double mandelbrot(Complex);
 // Gives color to a given pixel
 void colour_pixel(sf::VertexArray*,long double);
 
